4e82e01f2572635c60ccaa74c65c604e2906961f
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-09-29 20:52:10 -0600

    se añade todo lo del formulario, aunque el js puede estar mal

---
+++ 04Formulario/js/validacion.js
@@ -0,0 +1,46 @@
+/*
+JavaScript es un lenguaaje multiparadigma
+Acepta la programacion funcional, estructurada, POO, Eventos
+Dentro de JS, no existe el typado de variables (int, string, float, etc)
+Solo existen 3 tipos de variables de acuerdo al estandar ES6
+VAR, LET y CONST
+*/
+
+function validar(formulario) {
+    //quiero validar que el campo nombre acepte mas de 3 caracteres
+    if (formulario.Nombre.value.length < 4) {
+        alert("Porfavor escribe más de 3 caracteres en el campo nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+
+    //validacio para unicamente letras
+    var checkStr = formulario.Nombre.value;
+    alert(checkStr);
+
+    var abcOk = "QWERTYUIOPASDFGHJKLÑZXCVBNM" + "qwertyuiopasdfghjklñzxcvbnm";
+
+    var allValido = true;
+
+    //tenemos que comparar la cadena de nombre vs abc
+
+    for (var i = 0; i < checkStr.length; i++) {
+        var caracteres = checkStr.charAt(i);
+        for (var j = 0; j < abcOk.length; j++) {
+            if (caracteres == abcOk.charAt(j)) {
+                break;
+            }
+        }
+        if (j == abcOk.length) {
+            allValido = false;
+            break;
+        }
+    }
+
+    
+    if(!allValido){
+        alert("Escribe unicamente letras en el campo nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+}
