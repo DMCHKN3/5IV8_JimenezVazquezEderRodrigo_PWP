df1f69581bae152261361af98b65bb81a8fc2234
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-09-29 23:21:30 -0600

    se vuelve a subir lo mismo porque no se guardo

--- 04Formulario/js/validacion.js
+++ 04Formulario/js/validacion.js
@@ -43,4 +43,43 @@ function validar(formulario) {
         formulario.nombre.focus();
         return false;
     }
+    
+    var checkStr = formulario.Edad.value;
+    alert(checkStr);
+
+    var abcOk = "1234567890" + "qwertyuiopasdfghjklñzxcvbnm" + " ";
+
+    var allValido = true;
+
+    //tenemos que comparar la cadena de nombre vs abc
+
+    for (var i = 0; i < checkStr.length; i++) {
+        var caracteres = checkStr.charAt(i);
+        for (var j = 0; j < abcOk.length; j++) {
+            if (caracteres == abcOk.charAt(j)) {
+                break;
+            }
+        }
+        if (j == abcOk.length) {
+            allValido = false;
+            break;
+        }
+    }
+
+    if(!allValido){
+        alert("Escribe unicamente digitos en el campo edad");
+        formulario.Edad.focus();
+        return false;
+    }
+
+    //vamos a crear una funcion de una expresion regular para validar el correo electronico
+    //texto.texto@texto.texto
+
+    var b = /^[^@\s]+[^@\.\s]+(\.[^@\.\s]+)+$/;
+
+    var txt = formulario.Correo.value;
+
+    alert("Email" + (b.test(txt)? " ": " no ") + "valido");
+
+    return b.test;
 }
