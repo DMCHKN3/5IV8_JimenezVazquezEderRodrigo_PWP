8a28837fbbad2765bade4b6c96fa461259b0255c
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-10-08 00:31:41 -0600

    se crea la funcion para calcular la edad

--- 05 Ejercicios/ejerciciosps/js/funciones6.js
+++ 05 Ejercicios/ejerciciosps/js/funciones6.js
@@ -5,3 +5,13 @@ function validara(e){
     var codigo = String.fromCharCode(teclado);
     return patron.test(codigo);
 }
+
+function calcular(){
+    var añoactal = new Date().getFullYear();
+    var añonac = document.getElementById("nacimiento").value;
+    if(añoac < 1900 || añonac > añoactal){
+        alert("Año no valido");
+        return;
+    }
+    var edad = añoactal - añonac;
+}
