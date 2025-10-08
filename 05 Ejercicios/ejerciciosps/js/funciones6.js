503a2aee73cae67bed778084b21e6b76f27ae34c
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-10-08 13:40:42 -0600

    en el archivo 5 y 6 de js, se arregla un bug ya que se aceptan letras

--- 05 Ejercicios/ejerciciosps/js/funciones6.js
+++ 05 Ejercicios/ejerciciosps/js/funciones6.js
@@ -1,7 +1,7 @@
 function validara(e){
     var teclado = (document.all) ? e.keyCode : e.which;
     if (teclado == 8) return true;
-    var patron = /[0-9\ d]/;
+    var patron = /[0-9\d]/;
     var codigo = String.fromCharCode(teclado);
     return patron.test(codigo);
 }
