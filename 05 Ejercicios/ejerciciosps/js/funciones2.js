ab820c68cdb57c5245f9db19913b56c6dd57a2a1
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-10-11 14:40:31 -0600

    se crean las funciones para el ejerccicio 2

---
+++ 05 Ejercicios/ejerciciosps/js/funciones2.js
@@ -0,0 +1,35 @@
+function validar(e) {
+
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9\ .]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+
+}
+
+function calcular() {
+    var v1 = document.getElementById("venta1").value;
+    var v2 = document.getElementById("venta2").value;
+    var v3 = document.getElementById("venta3").value;
+    var sueldobase = document.getElementById("sueldob").value;
+
+    if (v1 == "" || v2 == "" || v3 == "" || sueldobase == "") {
+        alert("Favor de llenar todos los campos");
+    }
+    else if (v1 < 0 || v2 < 0 || v3 < 0 || sueldobase < 0) {
+        alert("Favor de inresar valores mayores o iguales a 0");
+    }
+
+    var totalventas = (parseFloat(v1) * 0.10) + (parseFloat(v2) * 0.10) + (parseFloat(v3) * 0.10);
+    totalventas = parseFloat(totalventas.toFixed(2));
+    
+    var sueldototal = parseFloat(sueldobase) + totalventas;
+    sueldototal = parseFloat(sueldototal.toFixed(2));
+}
+
+function borrar() {
+    document.getElementById("saldoi").value = " ";
+    document.getElementById("cantidadi").value = " ";
+    document.getElementById("mesesi").value = " ";
+}
