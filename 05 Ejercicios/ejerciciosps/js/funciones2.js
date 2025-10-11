732a210bfb460e5a55deb6941627d6041a13c653
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-10-11 15:51:50 -0600

    se añaden las variables de cada comision asi como las lineas de codigo correspondientes para mostrar y eliminar

--- 05 Ejercicios/ejerciciosps/js/funciones2.js
+++ 05 Ejercicios/ejerciciosps/js/funciones2.js
@@ -21,12 +21,19 @@ function calcular() {
         alert("Favor de inresar valores mayores o iguales a 0");
     }
 
-    var totalventas = (parseFloat(v1) * 0.10) + (parseFloat(v2) * 0.10) + (parseFloat(v3) * 0.10);
+    var coms1 = parseFloat(v1) * 0.10;
+    var coms2 = parseFloat(v2) * 0.10;
+    var coms3 = parseFloat(v3) * 0.10;
+
+    var totalventas = coms1 + coms2 + coms3;
     totalventas = parseFloat(totalventas.toFixed(2));
     
     var sueldototal = parseFloat(sueldobase) + totalventas;
     sueldototal = parseFloat(sueldototal.toFixed(2));
 
+    document.getElementById("com1").value = "$ " + coms1;
+    document.getElementById("com2").value = "$ " + coms2;
+    document.getElementById("com3").value = "$ " + coms3;
     document.getElementById("sueldof").value = "$ " + sueldototal;
 }
 
@@ -36,4 +43,7 @@ function borrar() {
     document.getElementById("venta3").value = " ";
     document.getElementById("sueldob").value = " ";
     document.getElementById("sueldof").value = " "; 
+    document.getElementById("com1").value = " ";
+    document.getElementById("com2").value = " ";
+    document.getElementById("com3").value = " ";
 }
