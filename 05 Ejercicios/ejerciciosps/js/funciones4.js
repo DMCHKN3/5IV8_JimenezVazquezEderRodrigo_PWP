0c26743262e30e433f10eb544a03b030ef40daae
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-10-11 23:01:41 -0600

    se crean las funciones para el ejercicio 4

---
+++ 05 Ejercicios/ejerciciosps/js/funciones4.js
@@ -0,0 +1,56 @@
+function validar(e){
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9\ .]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function calificar(){
+    var cal1 = document.getElementById("calf1").value;
+    var cal2 = document.getElementById("calf2").value;
+    var cal3 = document.getElementById("calf3").value;
+    var trabajo = document.getElementById("trabfin").value;
+    var examen = document.getElementById("exam").value;
+
+    if(cal1 == "" || cal2 == "" || cal3 == "" || trabajo == "" || examen == ""){
+        alert("Favor de llenar todos los campos");
+        return;
+    }
+    else if(cal1 < 0 || cal2 < 0 || cal3 < 0 || trabajo < 0 || examen < 0){
+        alert("Favor de ingresar valores mayores o iguales a 0");
+        return;
+    }
+    else if(cal1 > 10 || cal2 > 10 || cal3 > 10 || trabajo > 10 || examen > 10){
+        alert("Favor de ingresar valores menores a 10 y mayores a 0");
+        return;
+    }
+
+    cal1 = parseFloat(cal1);
+    cal2 = parseFloat(cal2);
+    cal3 = parseFloat(cal3);
+    trabajo = parseFloat(trabajo);
+    examen = parseFloat(examen);
+
+    var promcal = ((cal1+cal2+cal3)*55)/30;
+    var caltrabajo = (trabajo*15)/10;
+    var calexamen = (examen * 30)/10;
+
+    promcal = promcal.toFixed(2);
+    caltrabajo = caltrabajo.toFixed(2);
+    calexamen = calexamen.toFixed(2);
+
+    var califfinal = promcal + caltrabajo + calexamen
+    califfinal = (califfinal.toFixed(2))/10;
+
+    document.getElementById("califfin").value = califfinal;
+}
+
+function borrar(){
+    document.getElementById("calf1").value = " ";
+    document.getElementById("calf2").value = " ";
+    document.getElementById("calf3").value = " ";
+    document.getElementById("trabfin").value = " ";
+    document.getElementById("exam").value = " ";
+    document.getElementById("califfin").value = " ";
+}
