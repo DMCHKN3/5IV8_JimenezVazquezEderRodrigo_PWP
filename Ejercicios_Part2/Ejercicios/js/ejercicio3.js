d0f938040db2d5e8f853eb5889a77c5f49b74332
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-10-18 19:52:43 -0600

    haen las funciones para el ejercicio 3

---
+++ Ejercicios_Part2/Ejercicios/js/ejercicio3.js
@@ -0,0 +1,25 @@
+function verificar (e){
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function calcular(){
+    var horas = parseFloat(document.getElementById("hrsTrabajadas").value);
+    var pago = parseFloat(document.getElementById("pago").value);
+    var pagototal = 0;
+
+    var horasExtra = horas - 40;
+
+    if(horas > 40 || horasExtra > 0 || horasExtra <= 8){
+        pagototal = (40 * pago) + (horasExtra * pago * 2);
+    }else if(horas > 40 || horasExtra > 0 || horasExtra > 8 ){
+        pagototal = (40 * pago) + (8 * pago * 2) + ((horasExtra - 8) * pago * 3);
+    }else{
+        pagototal = horas * pago;
+    }
+
+    document.getElementById("resultado").value = pagototal;
+}
