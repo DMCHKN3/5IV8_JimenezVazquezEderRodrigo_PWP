113f1f04de1d3422219353e344a976f95664ffca
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-11-05 02:34:32 -0600

    le pedi ayuda a chat para terminar de arreghlar la pokedex pues tenia incongruencias entre las id de html y js (aunque creo que agrego cosas de mas :c)

--- 11 API Fetch/js/fetch.js
+++ 11 API Fetch/js/fetch.js
@@ -25,20 +25,21 @@ const pokedex = () => {
     let currentClassType = null;
 
     //tiene que cambiar los elementos de la img, para ello tenemos que crear un template que se encargue de encadenar los datos
-    const imageTemplate = "<img class = 'pokedisplay' src='imgSrc' alt='pokedisplay'/>";
+    const imageTemplate = "<img class = 'pokedisplay' src='{imgSrc}' alt='pokedisplay'/>";
 
     //necesitamos un objto que se encargue de guardar las rutas de las imgs q vamos a cambiar dependiendo de si es una busqueda, lo encontro o no al pokemon
-
+    // rutas relativas resueltas desde el html, no desde el archivo js
     const images = {
-        imgPokemonNotFound: "../img/404.png",
-        imgLoading: "../img/loading.gif"
+        imgPokemonNotFound: "./img/404.png",
+        imgLoading: "./img/loading.gif"
     };
 
     //necesitamos una variable que guarde todos los contenedores de la pokedex
 
     const containers = {
         imagenContainer: document.getElementById("pokedisplay-container"),
-        pokemonTypesContainer: document.getElementById("pokemonTypes"),
+        // coincide con el id en tu api.html
+        pokemonTypesContainer: document.getElementById("pokemon-types-container"),
         pokemonNameElement: document.getElementById("pokemonNameResult"),
         pokemonAbilitiesElement: document.getElementById("pokemonAbilities"),
         pokemonMovesElement: document.getElementById("pokemonMoves"),
@@ -61,58 +62,70 @@ const pokedex = () => {
     // funciones mas pequeñas que sin importar el orden puedan obtener cada uno de los datos solicitados
 
     const processPokemonType = (pokemonData) => {
-        //primero necesitamos obtener el tipo de pokemon, el nombre y la clase para que se modifique en el html, una vez con ello tenemos que obtener los stats, movs, habs
+        let pokemonTypeHtml = "";
+        const firstClass = pokemonData.types?.[0]?.type?.name || "";
 
-        let pokemonType = "";
-        //utilizo una busqueda de la clase de pokemon, eso se refiere al tipo de pokemon
-
-        const firstClass = pokemonData.types[0].type.name;
-
-        pokemonData.types.forEach((pokemonTypeData) => {
-            //necesito tener la etiqueta de cada cambio
-            pokemonType += `<span class = "pokemon-type ${pokemonData.type.name}"> ${pokemonTypeData.type.name}</span>`
+        pokemonData.types?.forEach((pokemonTypeData) => {
+            pokemonTypeHtml += `<span class="pokemon-type ${pokemonTypeData.type.name}">${pokemonTypeData.type.name}</span>`;
         });
-        //para poder quitar y cambiar el contenedor dependiendo del tipo tengo que saber a cual pertenece
 
-        if (currentClassType) {
-            containers.pokemonMovesElement.classList.remove(currentClassType);
-            containers.pokemonAbilitiesElement.classList.remove(currentClassType);
-        }//ahora tengo que agregar lo nuevo
-        containers.pokemmonMovesElement.classList.add(firstClass);
-        containers.pokemonAbilitiesElement.classList.add(firstClass);
-        //debo de agregar las etiquetas creadas dentro del forEach
+        if (containers.pokemonMovesElement && containers.pokemonAbilitiesElement) {
+            if (currentClassType) {
+                containers.pokemonMovesElement.classList.remove(currentClassType);
+                containers.pokemonAbilitiesElement.classList.remove(currentClassType);
+            }
+            if (firstClass) {
+                containers.pokemonMovesElement.classList.add(firstClass);
+                containers.pokemonAbilitiesElement.classList.add(firstClass);
+                currentClassType = firstClass;
+            }
+        }
 
-        containers.pokemonTypesContainer.innerHTML = pokemonType;
+        if (containers.pokemonTypesContainer) {
+            containers.pokemonTypesContainer.innerHTML = pokemonTypeHtml;
+        }
     };
 
     //ahora necesitamos obtener las stats del pokemon
     const processPokemonStats = (pokemonData) => {
         pokemonData.stats?.forEach((pokemonStatData) => {
-            //vamos a evaluar si encuentra el nombre de la stat para colocarlo en su container correspondiente
+            const pct = Math.min(100, pokemonStatData.base_stat);
             switch (pokemonStatData.stat.name) {
                 case "hp":
-                    pokemonStatsElements.hp.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.hp.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%);`;
+                    if (pokemonStatsElements.hp) {
+                        pokemonStatsElements.hp.innerHTML = pokemonStatData.base_stat;
+                        pokemonStatsElements.hp.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pct}%, rgba(0,0,0,1) ${pct}%);`;
+                    }
                     break;
                 case "attack":
-                    pokemonStatsElements.attack.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.attack.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%);`;
+                    if (pokemonStatsElements.attack) {
+                        pokemonStatsElements.attack.innerHTML = pokemonStatData.base_stat;
+                        pokemonStatsElements.attack.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pct}%, rgba(0,0,0,1) ${pct}%);`;
+                    }
                     break;
                 case "defense":
-                    pokemonStatsElements.defense.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.defense.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%);`;
+                    if (pokemonStatsElements.defense) {
+                        pokemonStatsElements.defense.innerHTML = pokemonStatData.base_stat;
+                        pokemonStatsElements.defense.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pct}%, rgba(0,0,0,1) ${pct}%);`;
+                    }
                     break;
                 case "special-attack":
-                    pokemonStatsElements.specialAttack.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.specialAttack.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%);`;
+                    if (pokemonStatsElements.specialAttack) {
+                        pokemonStatsElements.specialAttack.innerHTML = pokemonStatData.base_stat;
+                        pokemonStatsElements.specialAttack.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pct}%, rgba(0,0,0,1) ${pct}%);`;
+                    }
                     break;
                 case "special-defense":
-                    pokemonStatsElements.specialDefense.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.specialDefense.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%);`;
+                    if (pokemonStatsElements.specialDefense) {
+                        pokemonStatsElements.specialDefense.innerHTML = pokemonStatData.base_stat;
+                        pokemonStatsElements.specialDefense.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pct}%, rgba(0,0,0,1) ${pct}%);`;
+                    }
                     break;
                 case "speed":
-                    pokemonStatsElements.speed.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.speed.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%);`;
+                    if (pokemonStatsElements.speed) {
+                        pokemonStatsElements.speed.innerHTML = pokemonStatData.base_stat;
+                        pokemonStatsElements.speed.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pct}%, rgba(0,0,0,1) ${pct}%);`;
+                    }
                     break;
             }
         });
@@ -124,7 +137,9 @@ const pokedex = () => {
         pokemonData.abilities?.forEach((PokemonAbilityData) => {
             pokemonAbilitiesContent += `<li>${PokemonAbilityData.ability.name}</li>`;
         });
-        containers.pokemonAbilitiesContainer.innerHTML = pokemonAbilitiesContent;
+        if (containers.pokemonAbilitiesElement) {
+            containers.pokemonAbilitiesElement.innerHTML = pokemonAbilitiesContent;
+        }
     };
 
     const processPokemonMoves = (pokemonData) => {
@@ -132,12 +147,16 @@ const pokedex = () => {
         pokemonData.moves?.forEach((PokemonMoveData) => {
             pokemonMovesContent += `<li>${PokemonMoveData.move.name}</li>`;
         });
-        containers.pokemonMovesContainer.innerHTML = pokemonMovesContent;
+        if (containers.pokemonMovesElement) {
+            containers.pokemonMovesElement.innerHTML = pokemonMovesContent;
+        }
     };
 
     //necesito poner la img de cargando y que tambien se deshabiliten los botones
     const setLoading = () => {
-        containers.imagenContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgLoading);
+        if (containers.imagenContainer) {
+            containers.imagenContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgLoading);
+        }
         buttons.all.forEach((button) => {
             button.disabled = true;
         });
@@ -148,91 +167,96 @@ const pokedex = () => {
         buttons.all.forEach(button => checkDisabled(button));
     };
 
-    //Vamos a crear una promesa, para poder obtener cada uno de los elementos de la pokedex, pero sin importar el orden, 
-    // significa que cuando se realice la peticion va a ser de tipo asincrona, eso significa que va a atender 
-    // sin importar el orden de la transferencia de los paquetes los datos del request, 
-    // los va a procesar y despues armar, para ello utilizaremos una funcion de tipo fetch la cual como argumento 
-    // principal va a necesitar la url del api y despues una serie de then para procesar los datos
-
     const getPokemonData = async (pokemonName) => fetch(`${pokeApiURL}pokemon/${pokemonName}`, {
-        //cualquier peticion fetch por defecto es de tipo GET, pero si queremmos hacer otro tipo de peticiones tenemos que especificarlo en el segundo argumento. Pero cuando sea de una BD entonces ta podemos modificar el tipo de metodo PUT, DELETE, etc.}
-        //Despues del metodo es necesario el tipo de encabezado, las cabecesar son necesarias para que el sv entienda que tipo de datos le estamos enviando y que tipo de datos esperamos recibir
         method: "GET",
         headers: {
             "Content-Type": "application/json"
         }
-        //si por ejemplo tiene elementos de formulario dentro del bodym aqui se deben de incluir
-        //body: JSON.stringify({argumento})
     })
-    .then((res) => res.json()) //se convierte la respuesta en un obj json
-    .catch((error) => ({ requestFailed: true }));
+    .then((res) => {
+        if (!res.ok) throw new Error("request failed");
+        return res.json();
+    })
+    .catch(() => ({ requestFailed: true }));
 
     //necesitamos validar si se debe habilitar o deshabilitar los botones
     const checkDisabled = (button) => {
-        //para cuando exista un id negativo
-        button.disabled = button.id === "btnDown" && containers.pokemonIdElement.value <= 1;
+        const idNumber = Number(containers.pokemonIdElement?.value || 0);
+        if (button.id === "btnDown") {
+            button.disabled = idNumber <= 1;
+        } else {
+            button.disabled = false;
+        }
     };
 
     //mientras ocurre el fecth necesitamos una funcion que vaya armando los datos de la poedex, entonces necesitamos validar el ID o el nombre del pokemon
     const setPokemonData = async (pokemonName) => {
-        if (pokemonName) {
-            //poner la img de bvusqueda y deshabilitar
+        if (pokemonName || pokemonName === 0) {
             setLoading();
 
-            //debo de armar la consulta pra determinar el orden de los datos
-            const pokemonData = await getPokemonData(typeof pokemonName === "" ? pokemonName.toLowerCase() : pokemonName);
-            //validar si la peticion fallo
+            const query = (typeof pokemonName === "string") ? pokemonName.toLowerCase().trim() : pokemonName;
+            const pokemonData = await getPokemonData(query);
+
             if (pokemonData.requestFailed) {
-                containers.imagenContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgPokemonNotFound);
+                if (containers.imagenContainer) containers.imagenContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgPokemonNotFound);
+                if (containers.pokemonNameElement) containers.pokemonNameElement.innerHTML = "";
+                if (containers.pokemonIdElement) containers.pokemonIdElement.value = 0;
+                if (containers.pokemonTypesContainer) containers.pokemonTypesContainer.innerHTML = "";
+                if (containers.pokemonMovesElement) containers.pokemonMovesElement.innerHTML = "";
+                if (containers.pokemonAbilitiesElement) containers.pokemonAbilitiesElement.innerHTML = "";
             } else {
-                //ponemos todos los elementos
-                containers.imagenContainer.innerHTML = `${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_default)} 
-        ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_shiny)}`;
+                if (containers.imagenContainer) {
+                    containers.imagenContainer.innerHTML = `${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_default || images.imgPokemonNotFound)} 
+        ${imageTemplate.replace("{imgSrc}", pokemonData.sprites.front_shiny || pokemonData.sprites.front_default || images.imgPokemonNotFound)}`;
+                }
+                if (containers.pokemonNameElement) containers.pokemonNameElement.innerHTML = pokemonData.name;
+                if (containers.pokemonIdElement) containers.pokemonIdElement.value = pokemonData.id;
 
-                containers.pokemonNameElement.innerHTML = pokemonData.name;
-                containers.pokemonIdElement.innerHTML = pokemonData.id;
-
-                //repartir los demas elementos
                 processPokemonType(pokemonData);
                 processPokemonStats(pokemonData);
                 processPokemonAbilities(pokemonData);
                 processPokemonMoves(pokemonData);
-
             }
             setLoadingComplete();
+        } else {
+            if (typeof Swal !== "undefined") {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error en tu busqueda',
+                    text: 'Por favor ingresa un nombre de Pokémon primero',
+                    confirmButtonText: 'Aceptar'
+                });
+            } else {
+                alert("Por favor ingresa un nombre de Pokémon primero");
+            }
         }
-        else {
+    };
 
-            //cuando exista una alerta o un error
-            Swal.fire({
-                icon: 'error',
-                title: 'Error en tu busqueda',
-                text: 'Por favor ingresa un nombre de Pokémon primero',
-                confirmButtonText: 'Aceptar'
-            });
-
+    //la ultima funcion se encarga de vincular todas las busquedas
+    const trigger = () => {
+        if (buttons.search) {
+            buttons.search.onclick = () => setPokemonData(pokemonInput?.value);
         }
-
-        //la ultima funcion se encarga de vincular todas las busquedas
-        const trigger = () => {
-            buttons.search.onclick = () => setPokemonData(pokemonInput.value);
-            //orientemos el evento de ese click
+        if (pokemonInput) {
             pokemonInput.onkeyup = (event) => {
                 event.preventDefault();
                 if (event.key === "Enter") {
                     setPokemonData(pokemonInput.value);
                 }
             };
-            buttons.next.onclick = () => { setPokemonData(containers.pokemonIdElement.value + 1) };
-            buttons.previous.onclick = () => { setPokemonData(containers.pokemonIdElement.value - 1) };
-        };
-
-        setLoadingComplete();
-        trigger();
+        }
+        if (buttons.next) {
+            buttons.next.onclick = () => { setPokemonData(Number(containers.pokemonIdElement?.value || 0) + 1); };
+        }
+        if (buttons.previous) {
+            buttons.previous.onclick = () => { setPokemonData(Math.max(1, Number(containers.pokemonIdElement?.value || 1) - 1)); };
+        }
     };
 
-    
+    setLoadingComplete();
+    trigger();
 };
+// end pokedex
 
 window.onload = pokedex;
 
