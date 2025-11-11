2a25f8fb318ae1710340193f43804247e4475d2b
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-11-10 20:39:44 -0600

    lo hice a como lo entendi jeje profe jeje

--- 13 Ejerciciof 2o par/actividad.js
+++ 13 Ejerciciof 2o par/actividad.js
@@ -1,26 +1,23 @@
-function memorizar(fn) {
+function memo(a, b) {
     let cache = {};
     let rep = 0;
+    
+    let operacion = (a,b) => { return a+b; };
 
-        let operacion = (a, b) => { return a + b; };
-
-        return function (a, b) {
-            let key = a + ',' + b;
-            if (!(key in cache)) {
-                cache[key] = operacion(a, b);
-                rep++;
-            }
-            return cache[key] + ", llamadas: " + rep;
-        };
+    return function (a, b) {
+        let key = a + ',' + b;
+        if (!(key in cache)) {
+            cache[key] = operacion(a, b);
+            rep++;
+        }
+        return cache[key] + ", llamadas: " + rep;
     };
-
-    return inputs.map();
 }
 
 let sumaMemo = memo();
 
-console.log(sumaMemo([2, 2], [2, 2], [4, 2]));
-console.log(sumaMemo([3, 4], [3, 4], [6, 4]));
+console.log(sumaMemo(3, 4));
+console.log(sumaMemo(3, 4));
 console.log(sumaMemo(5, 6));
 console.log(sumaMemo(3, 4));
 console.log(sumaMemo(5, 6));
