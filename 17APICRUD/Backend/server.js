0bf55acdc0d77511ae375a5cda32d0d4105f57d3
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-12-03 18:46:57 -0600

    se mete todo lo q no meti antier y todo lo de ayer

--- 17APICRUD/Backend/server.js
+++ 17APICRUD/Backend/server.js
@@ -1,7 +1,7 @@
 //se usa la estructura js 8
 import express from 'express';
 import path from 'path';
-
+import productRoutes from './routes/productroutes.js';
 //aqui nosotros tenemos que agregar las rutas que se van a consumir
 
 //levantar el servicio
@@ -16,6 +16,9 @@ app.use(express.static(path.join(__dirname, '../Frontend', 'public')));
 
 app.set('views engine', 'ejs');
 app.set('public', path.join(__dirname, '../Frontend', 'public'));
+
+//vamos a consumir las rutas
+app.use('/', productRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
